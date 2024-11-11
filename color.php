cf444e0bb7cd3e8dd6734c1bfb49d7045467377c
Lucas <105754327+gen-x-coder@users.noreply.github.com>
2024-11-11 17:57:51 +0100

    Create color.php

---
+++ color.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Color Indexer</title>
+</head>
+<body>
+    <h1>Color Indexer</h1>
+    <div id="color-list"></div>
+
+    <script src="color-indexer.js"></script>
+</body>
+</html>
